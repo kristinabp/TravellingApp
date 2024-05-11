9d6b6993e23ffa342408e7d6a9b641a56d9d7aae
Kristina Popova <krp@milestone.dk>
2024-05-11 12:09:28 +0300

    fix frontend

--- TravellingApp.UI/src/app/features/HomePage.tsx
+++ TravellingApp.UI/src/app/features/HomePage.tsx
@@ -1,97 +1,218 @@
 import React, { useState } from "react";
-import './styles/HomePage.css';
+import "./styles/HomePage.css";
+import TextField from "@mui/material/TextField";
+import { Box, Button } from "@mui/material";
+import dayjs, { Dayjs } from "dayjs";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
 const Home = () => {
   const [city, setCity] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [endDate, setEndDate] = useState("");
+  let hasData: boolean = false;
+
+  const [startDate, setStartDate] = React.useState<Dayjs | null>(
+    dayjs("2022-04-17T15:30")
+  );
+  const [endDate, setEndDate] = React.useState<Dayjs | null>(
+    dayjs("2022-04-17T15:30")
+  );
 
   const handleCityChange = (event) => {
     setCity(event.target.value);
   };
- 
-  const handleStartDateChange = (event) => {
-    setStartDate(event.target.value);
+
+  const goToHotelsPage = () => {
+    window.location.href = "";
   };
 
-  const handleEndDateChange = (event) => {
-    setEndDate(event.target.value);
+  const goToMonumentsPage = () => {
+    window.location.href = "";
+  };
+
+  const goReantACarPage = () => {
+    window.location.href = "";
+  };
+
+  const goToRestaurantsPage = () => {
+    window.location.href = "";
+  };
+
+  const goToGoogleMapsPage = () => {
+    window.location.href = "";
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-
-    fetch('http://localhost:3050/restaurantsData', {
-      method: 'POST',
+    fetch("http://localhost:3050/restaurantsData", {
+      method: "POST",
       body: JSON.stringify({ city }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
 
-    fetch('http://localhost:3060/rentCarData', {
-      method: 'POST',
+    fetch("http://localhost:3060/rentCarData", {
+      method: "POST",
       body: JSON.stringify({ city, startDate, endDate }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
 
-    fetch('http://localhost:8008/monuments', {
-      method: 'POST',
+    /*fetch("http://localhost:8008/monuments", {
+      method: "POST",
       body: JSON.stringify({ city }),
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
 
-    fetch('http://localhost:8000/hotels', {
-      method: 'POST',
+    fetch("http://localhost:8000/hotels", {
+      method: "POST",
       body: JSON.stringify({ city, startDate, endDate }),
       headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
+        "Content-Type": "application/json",
+      },
+    });*/
+    hasData = true;
   };
 
   return (
-    <div>
-      <h1>Welcome to Home page</h1>
-      <form className="formStyle" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="city">City:</label>
-          <input
-            type="text"
-            id="city"
-            value={city}
-            onChange={handleCityChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="startDate">Start Date:</label>
-          <input
-            type="date"
-            id="startDate"
-            value={startDate}
-            onChange={handleStartDateChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="endDate">End Date:</label>
-          <input
-            type="date"
-            id="endDate"
-            value={endDate}
-            onChange={handleEndDateChange}
-            required
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <>
+      <Box
+        component="form"
+        height={90}
+        my={4}
+        display="flex"
+        alignItems="center"
+        gap={4}
+        p={2}
+        sx={{ borderBottom: "1px solid grey" }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="City"
+          variant="outlined"
+          onChange={handleCityChange}
+        />
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer
+            components={["DateTimePicker", "DateTimePicker"]}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <DateTimePicker
+              label="Start date"
+              defaultValue={dayjs("2022-04-17T15:30")}
+              onChange={(newValue) => setStartDate(newValue)}
+            />
+          </DemoContainer>
+        </LocalizationProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DemoContainer
+            components={["DateTimePicker", "DateTimePicker"]}
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <DateTimePicker
+              label="End date"
+              defaultValue={dayjs("2022-04-17T15:30")}
+              onChange={(newValue) => setEndDate(newValue)}
+            />
+          </DemoContainer>
+        </LocalizationProvider>
+        <Button
+          sx={{ display: "flex" }}
+          variant="contained"
+          onClick={handleSubmit}
+        >
+          Search
+        </Button>
+      </Box>
+      {hasData && (
+        <>
+          <Box
+            component="form"
+            height={90}
+            my={4}
+            display="flex"
+            alignItems="center"
+            gap={4}
+            p={2}
+            sx={{ border: "1px solid grey" }}
+          >
+            <Button
+              sx={{ display: "flex" }}
+              variant="contained"
+              onClick={goToRestaurantsPage}
+            >
+              See more
+            </Button>
+          </Box>
+          <Box
+            component="form"
+            height={90}
+            my={4}
+            display="flex"
+            alignItems="center"
+            gap={4}
+            p={2}
+            sx={{ border: "1px solid grey" }}
+          >
+            <Button
+              sx={{ display: "flex" }}
+              variant="contained"
+              onClick={handleSubmit}
+            >
+              See more
+            </Button>
+          </Box>
+          <Box
+            component="form"
+            height={90}
+            my={4}
+            display="flex"
+            alignItems="center"
+            gap={4}
+            p={2}
+            sx={{ border: "1px solid grey" }}
+          >
+            <Button
+              sx={{ display: "flex" }}
+              variant="contained"
+              onClick={handleSubmit}
+            >
+              See more
+            </Button>
+          </Box>
+          <Box
+            component="form"
+            height={90}
+            my={4}
+            display="flex"
+            alignItems="center"
+            gap={4}
+            p={2}
+            sx={{ border: "1px solid grey" }}
+          >
+            <Button
+              sx={{ display: "flex" }}
+              variant="contained"
+              onClick={handleSubmit}
+            >
+              See more
+            </Button>
+          </Box>
+        </>
+      )}
+    </>
   );
 };
 
